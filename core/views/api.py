93de1ad7840208508ff7cf506927c7cfa2b43068
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2025-07-24 09:56:18 +0700

    fix: test IP

--- core/views/api.py
+++ core/views/api.py
@@ -15,21 +15,22 @@ def access_log_api(request):
         headers = get_headers_dict(request)
         path = request.META.get('PATH_INFO', '')
         
-        access_log = AccessLog.objects.create(
-            page_name=page_name,
-            ip_address=ip,
-            user_agent=user_agent,
-            referer=referer,
-            headers=headers,
-            request_path=path
-        )
+        if ip not in settings.TEST_IP:
+            access_log = AccessLog.objects.create(
+                page_name=page_name,
+                ip_address=ip,
+                user_agent=user_agent,
+                referer=referer,
+                headers=headers,
+                request_path=path
+            )
 
-        ip_data = get_ip_info(ip)
-        if ip_data:
-            access_log.country = ip_data.get('country')
-            access_log.country_code = ip_data.get('country_code')
-            access_log.ip_json = ip_data.get('data')
-            access_log.save()
+            ip_data = get_ip_info(ip)
+            if ip_data:
+                access_log.country = ip_data.get('country')
+                access_log.country_code = ip_data.get('country_code')
+                access_log.ip_json = ip_data.get('data')
+                access_log.save()
         
     return JsonResponse({
         'msg': 'ok'
